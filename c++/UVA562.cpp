8b26dfad79decb22a535de8261328c76c02faf4f
Zubair <zubairtusar17@gmail.com>
2022-06-18 22:31:13 +0600

    801 div2

--- c++/UVA562.cpp
+++ c++/UVA562.cpp
@@ -1,11 +1,25 @@
 #include<cstdio>
-#define SIZE 101
+#define SIZE1 101
+#define SIZE2 50001
 using namespace std;
 
 int M;
-int A[SIZE];
+int A[SIZE1];
 int Total;
 int Ans;
+int dp[SIZE1][SIZE2];
+
+void initDP()
+{
+    int i, j;
+    for(i=0; i<M; i++)
+    {
+        for(j=0; j<Total; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+}
 
 int abs(int n)
 {
@@ -18,20 +32,24 @@ int solve(int i, int sum1)
 {
     printf("##In solve## i: %4d  sum1: %4d\n", i, sum1);
     int sum2, left, right;
+    if (dp[i][sum1]!=-1)
+    {
+        return dp[i][sum1];
+    }
     if(i==M)
     {
         printf("##Returning## %d\n", abs(sum1-sum2));
         sum2 = Total-sum1;
-        return abs(sum1-sum2);
+        return dp[i][sum1] = abs(sum1-sum2);
     }
     printf("##Going Left##\n");
     left = solve(i+1, sum1);
     printf("##Going Right##\n");
     right = solve(i+1, sum1+A[i]);
     if(left<right)
-        return left;
+        return dp[i][sum1] = left;
     else
-        return right;
+        return dp[i][sum1] = right;
 }
 
 void readCase()
@@ -50,6 +68,7 @@ void solveCase()
     {
         Total += A[i];
     }
+    initDP();
     Ans = solve(0, 0);
 }
 
